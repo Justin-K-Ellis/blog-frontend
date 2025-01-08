dade8e5ea0b41efd9a9666099c1e600a1edc694a
Justin-K-Ellis <jlke45@gmail.com>
2025-01-08 22:44:46 +0900

    Update Navbar text.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import { NavLink } from "react-router";
 function Navbar() {
   return (
     <nav className="flex flex-row justify-between p-2">
-      <h1 className="text-3xl font-bold">Logical Space 🪐</h1>
+      <div className="flex flex-col">
+        <h1 className="text-3xl font-bold">Logical Space 🪐</h1>
+        <p className="italic font-light">
+          Exploring the space of reasons in philosophy and programming
+        </p>
+      </div>
       <ul className="flex flex-row text-xl gap-4">
         <li>
           <NavLink to={"/"}>Home</NavLink>
