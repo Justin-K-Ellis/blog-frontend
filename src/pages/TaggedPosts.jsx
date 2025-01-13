4495147af19670013a8d10b92b43b3e5347f6554
Justin-K-Ellis <jlke45@gmail.com>
2025-01-13 13:03:53 +0900

    Create page to preview all posts for a given tag.

--- src/pages/TaggedPosts.jsx
+++ src/pages/TaggedPosts.jsx
@@ -1,8 +1,47 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router";
 
+import PostPreviewCard from "../components/PostPreviewCard";
+import Title from "../components/Title";
+
 function TaggedPosts() {
+  const [taggedPosts, setTaggedPosts] = useState([]);
+  const [error, setError] = useState(false);
   const { tag } = useParams();
-  return <h2>All Posts for {tag}.</h2>;
+  const apiUri = import.meta.env.VITE_API + `/api/tags/name/${tag}`;
+
+  useEffect(() => {
+    async function getTaggedPosts() {
+      try {
+        const response = await fetch(apiUri);
+        const data = await response.json();
+        setTaggedPosts(data[0].posts);
+      } catch (error) {
+        console.log(`There was an error when fetching tagged posts. ${error}`);
+        setError(true);
+      }
+    }
+    getTaggedPosts();
+  }, [apiUri]);
+
+  if (error) return <p>Sorry, something went wrong :(</p>;
+  taggedPosts && console.log(taggedPosts);
+  console.log("tag:", tag);
+
+  return (
+    <>
+      <Title text={tag} />
+      <ul>
+        {taggedPosts.map((post) => {
+          return (
+            <li key={post.id}>
+              <PostPreviewCard post={post} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default TaggedPosts;
