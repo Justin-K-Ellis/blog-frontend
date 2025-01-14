6a9d4a08358aaa645a9bc581cb37e2edbad4bd68
Justin-K-Ellis <jlke45@gmail.com>
2025-01-14 19:53:06 +0900

    Improve layout of text, font

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,9 +2,11 @@ import Back from "../components/Back";
 
 function FourOhfour() {
   return (
-    <section>
-      <h2 className="text-3xl">404</h2>
-      <p className="text-2xl">Sorry, it looks like this page doesn't exist.</p>
+    <section className="flex flex-col items-center justify-center mt-6">
+      <h2 className="text-6xl font-bold mb-6">404</h2>
+      <p className="text-2xl text-center">
+        Sorry, it looks like this page doesn't exist.
+      </p>
       <Back />
     </section>
   );
